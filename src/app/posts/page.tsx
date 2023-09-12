f1ff70512fc8467d444e06b86a019dc272ac0f4e
Ben Gubler <nebrelbug@gmail.com>
2023-09-12 16:03:05 -0600

    chore: update deps, remove TS-ignore

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -4,8 +4,6 @@ export default async function Posts() {
   return (
     <div className="max-w-screen-lg m-auto">
       <h2 className="text-4xl font-bold mt-10">Posts</h2>
-      {/* Known error, being worked on upstream. TODO: remove after fix */}
-      {/* @ts-expect-error Async Server Component */}
       <PostsList limit={false} />
     </div>
   )

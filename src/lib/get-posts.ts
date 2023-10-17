f215823be0174d689867f3f9b434ab0f4de34c7e
Ben Gubler <nebrelbug@gmail.com>
2023-10-16 22:34:19 -0600

    chore: add new abbreviations and fix RegExp bug

--- src/lib/get-posts.ts
+++ src/lib/get-posts.ts
@@ -50,13 +50,16 @@ const speciallyCapitalizedTitles = [
   "vs",
   "DeepSpeed",
   "HPC",
-  "ML"
+  "ML",
+  "gom",
+  "nvidia-smi",
+  "TL;DR"
 ]
 
 let capitalizationOptions: Record<string, string> = {}
 
 for (let title of speciallyCapitalizedTitles) {
-  let escapedTitle = title.replace(/[-\/\\^$*+?.()|[\]{}]/gi, "\\$&")
+  let escapedTitle = title.replace(/[\/\\^$*+?.()|[\]{}]/gi, "\\$&")
 
   escapedTitle = escapedTitle.replace(/[A-Za-z]/g, function (match) {
     return "[" + match.toLowerCase() + match.toUpperCase() + "]"

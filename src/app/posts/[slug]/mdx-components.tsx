80a369e4d227320b81f5f238f0df2db1130a7075
Ben Gubler <nebrelbug@gmail.com>
2023-10-17 00:31:42 -0600

    fix: broken Image component in MDX

--- src/app/posts/[slug]/mdx-components.tsx
+++ src/app/posts/[slug]/mdx-components.tsx
@@ -1,14 +1,14 @@
 import type { MDXComponents } from "mdx/types"
 
 import { Code } from "bright"
-import Image from "next/image"
+import NextImage, { ImageProps } from "next/image"
 import Link from "next/link"
 
 const mdxComponents: MDXComponents = {
   // Override the default <a> element to use the next/link component.
   a: ({ href, children }) => <Link href={href as string}>{children}</Link>,
   pre: Code,
-  Image: Image
+  Image: (props: ImageProps) => <NextImage {...props} />
 }
 
 export { mdxComponents }
